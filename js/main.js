98bd98a48b9cc212a96fed3b14c741417378c982
Husain <husainahmmed1b@gmail.com>
2017-03-22 21:32:04 +0600

    add changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-//  JavaScript Code
+//  30 days to Learn jQuery by Jeffrey Way Form Tutplus.com
 (function() {
 	$('dd').filter(':nth-child(n+4)').addClass('hide');
 
-	$("dl").on('mouseenter', 'dt', function() {
+	$('dl').on('mouseenter', 'dt', function() {
 		$(this)
 			.next()
 				.slideDown(200)
 				.siblings('dd')
 					.slideUp(200);
 	});
-
 })();
